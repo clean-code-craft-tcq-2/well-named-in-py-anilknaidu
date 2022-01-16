040d9757f0beb9ba5c2581ca59773719a78b40f2
Naidu Anil Kumar (RBEI/EPV32-EC) <AnilKumar.Naidu@in.bosch.com>
2022-01-16 17:22:20 +0530

    Commit2 with code fixes

--- ColorPair25_Manual_Generator.py
+++ ColorPair25_Manual_Generator.py
@@ -1,5 +1,4 @@
 from ColorPair25_Lib import *
-import numpy as np
 import matplotlib.pyplot as plt
 
 def ColorPair25_GeneratePDF_Manual():
@@ -14,23 +13,16 @@ def ColorPair25_GeneratePDF_Manual():
             print("ColorCode: ",colorCode,"--> MajorColor: "+majorColor,"  MinorColor: "+minorColor)
             cp25_table_rows.append(colorCode)
             cp25_table_text.append([str(colorCode), majorColor,"",minorColor,""])
-            if(minorColor =="Slate"):
-                minorColor = "slategrey"
+            if(minorColor =="Slate"): minorColor = "slategrey"
             cp25_table_cell_colors.append(['White',"White",majorColor,"White",minorColor])
 
-    colorPair25_Fig, colorPair25_table_Axes = plt.subplots(figsize = (6,8))
+    colorPair25_Fig, colorPair25_table_Axes = plt.subplots(figsize = (6,8),)
 
     colorPair25_table_Axes.xaxis.set_visible(False) 
     colorPair25_table_Axes.yaxis.set_visible(False)
 
-    colorPair25_NumMapTable = colorPair25_table_Axes.table(cellText=cp25_table_text,
-                                        rowLabels=cp25_table_rows,
-                                        colLabels=cp25_columns,
-                                        cellColours=cp25_table_cell_colors,
-                                        loc='top',
-                                        bbox=[0,0,1,1])
-    colorPair25_NumMapTable.scale(2, 2)
+    colorPair25_table_Axes.table(cellText=cp25_table_text, rowLabels=cp25_table_rows, colLabels=cp25_columns, cellColours=cp25_table_cell_colors, loc='top', bbox=[0,0,1,1])
     colorPair25_plot_text = "Refer below table for ColorCode and corresponding Major and Minor Color"
     plt.gcf().text(0.03, 0.9, colorPair25_plot_text, fontsize=10)
-    plt.savefig("ColorPair25_Manual_forReference.pdf",bbox_inches='tight')
+    plt.savefig("ColorPair25_PDF_Manual_Generated.pdf",bbox_inches='tight')
     plt.show()
