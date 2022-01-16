ba0ef19aeb1cdc250a0a38c1a9391c5c5afb10fa
Naidu Anil Kumar (RBEI/EPV32-EC) <AnilKumar.Naidu@in.bosch.com>
2022-01-16 16:58:53 +0530

    Initial Draft commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ColorPair25_Lib import *
+from ColorPair25_Manual_Generator import ColorPair25_GeneratePDF_Manual
 
 def test_number_to_pair(pair_number,
                         expected_major_color, expected_minor_color):
@@ -9,7 +10,7 @@ def test_number_to_pair(pair_number,
 def test_pair_to_number(major_color, minor_color, expected_pair_number):
   pair_number = get_pair_number_from_color(major_color, minor_color)
   assert(pair_number == expected_pair_number)
-  
+
 if __name__ == '__main__':
   test_number_to_pair(4, 'White', 'Brown')
   test_number_to_pair(5, 'White', 'Slate')
@@ -17,3 +18,5 @@ if __name__ == '__main__':
   test_pair_to_number('Violet', 'Slate', 25)
   test_pair_to_number('Red', 'Orange', 7)
   print('Done :)')
+  ColorPair25_GeneratePDF_Manual()
+  print("PDF manual generated, it can be printed if required")
